b467dc117c862760b53a9f70a33478c3b6b3443f
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-04-19 01:14:45 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -13,13 +14,21 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // to parse incoming requests with JSON payloads
 app.use(cookieParser()); // to parse incoming requests with cookies
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
- 
+
+app.use(express.static(path.join(__dirname, '/frontend/dist'))) 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend' , 'dist' , 'index.html'))
+})
+
 server.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
